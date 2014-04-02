a099e11133252292a006e0ba8e8a9879f7e64664
darrenglore <darrenglore@gmail.com>
2014-04-02 17:09:12 -0500

    a

--- src/com/lrs/geoquiz/QuizActivity.java
+++ src/com/lrs/geoquiz/QuizActivity.java
@@ -1,26 +1,159 @@
 package com.lrs.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class QuizActivity extends ActionBarActivity {
+public class QuizActivity extends Activity {
+	
+	private static final String TAG = "QuizActivity";
 
+	private Button mTrueButton;
+	private Button mFalseButton; 
+	private ImageButton mNextButton;
+	private ImageButton mPrevButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans,true),
+			new TrueFalse(R.string.question_mideast,false),
+			new TrueFalse(R.string.question_africa,false),
+			new TrueFalse(R.string.question_americas,true),
+			new TrueFalse(R.string.question_asia,true),
+	};
+	
+	private int mCurrentIndex = 0;
+	
+	private void updateQuestion(){
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue){
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		
+		if (userPressedTrue == answerIsTrue){
+			messageResId = R.string.correct_toast;
+		}else{
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+		
+	}
+	
+	private void nextQuestion(){
+		mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+		updateQuestion();
+	}
+	
+	private void prevQuestion(){
+		mCurrentIndex = (mCurrentIndex - 1);
+		if (mCurrentIndex < 0){
+			mCurrentIndex = mQuestionBank.length - 1;
+		}
+		updateQuestion();
+	}
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
+		
+		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				nextQuestion();
+			}
+		});
+			
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				checkAnswer(true);
+			}
+		});
+		
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener(){
 
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+			}
+			
+		});
+		
+		mNextButton = (ImageButton)findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				nextQuestion();
+			}
+			
+		});
+		
+		mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+		mPrevButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				prevQuestion();	
+			}
+		});
+		
+		updateQuestion();
 		
 	}
 
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG,"onStart() called");
+	}
+	
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG,"onPause() called");
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG,"onResume() called");
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG,"onStop() called");
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG,"onDestroy() called");
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
